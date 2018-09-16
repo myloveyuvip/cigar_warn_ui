38079be9abd5e0f70c0e16712e851db1e760551c
myloveyuvip <myloveyuvip@qq.com>
2018-09-17 00:07:17 +0800

    header去掉github链接

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   proxy: {
     '/apis': {    //将www.exaple.com印射为/apis
-      target: 'http://localhost:8080',  // 接口域名
+      target: 'http://localhost:8081',  // 接口域名
       secure: false,  // 如果是https接口，需要配置这个参数
       changeOrigin: true,  //是否跨域
       pathRewrite: {
